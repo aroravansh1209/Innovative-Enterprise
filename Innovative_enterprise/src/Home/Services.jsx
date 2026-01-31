84f5ab4453cf33dc9034a3321e1995733290d614
aroravansh1209 <109975931+aroravansh1209@users.noreply.github.com>
2026-01-31 20:39:53 +0530

    images

--- Innovative_enterprise/src/Home/Services.jsx
+++ Innovative_enterprise/src/Home/Services.jsx
@@ -17,6 +17,7 @@ import InternetLeasedLines from '../assets/LeaseLine.png'
 import MultiScreenSetup from '../assets/Multiscreensetup.png'
 import CCTVSurveillance from '../assets/CCTVSurveillance.png'
 import Cybersecurity from '../assets/Cybersecurity.png'
+import Webdev from '../assets/WebDev.png'
 
 export default function BusinessServices() {
   const [currentSlide, setCurrentSlide] = useState(0)
@@ -158,7 +159,7 @@ export default function BusinessServices() {
       title: "Website Development",
       category: "DEVELOPMENT",
       description: "Enterprise-grade cybersecurity and threat protection.",
-      image: Cybersecurity,
+      image: WebDev,
     },
     {
       id: 17,
